420c6ff7428df9ad1c36ccfea19797fc8a1baee3
FeedzOne <thekubsonyt@gmail.com>
2023-06-11 19:03:52 -0700

    updating the api to correctly show the images

--- drfstorecrud/settings.py
+++ drfstorecrud/settings.py
@@ -121,11 +121,12 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    "/var/www/static/"
 ]
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "media/"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
